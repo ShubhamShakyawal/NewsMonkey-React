2071e401c72c051ebf7f222739e0292064f0416d
ShubhamShakyawal <shakyawals@gmail.com>
2021-12-28 15:46:22 +0530

    Infinite scroll and top loading bar added

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -6,6 +6,14 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card">
+                    <div style={{
+                        display:'flex',
+                        justifyContent:'flex-end',
+                        position:'absolute',
+                        right:'0'
+                    }}>
+                    <span className="badge rounded-pill bg-danger" >{source} </span>
+                    </div>
                     <img src={!imageUrl?"https://us.123rf.com/450wm/pavelstasevich/pavelstasevich1811/pavelstasevich181101027/112815900-no-image-available-icon-flat-vector.jpg?ver=6":imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                         <h5 className="card-title">{title}</h5>
@@ -13,7 +21,7 @@ export class NewsItem extends Component {
                         <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                         <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
                     </div>
-                    <span className="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left:'90%',zIndex:1}}>{source} </span>
+                    
                 </div>
                          
             </div>
